0ceb22c3a648d1dc1b7b3962f89268dc88e2964d
Onur <69056862+ogunec@users.noreply.github.com>
2022-11-25 01:46:27 +0300

    Add files via upload

---
+++ TriangleDraw/TriangleDraw/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Kenar Uzunluğu: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int sayac = 1;
+int sayac2 = length;
+for (int i = 0; i < length; i++)
+{
+	for (int j = sayac2-1; j > 0; j--)
+	{
+		Console.Write(" ");
+	}
+	sayac2--;
+	for (int k = 1; k <= sayac; k++)
+	{
+		Console.Write("*");
+		Console.Write(" ");
+	}
+	sayac++;
+	Console.WriteLine();
+}
