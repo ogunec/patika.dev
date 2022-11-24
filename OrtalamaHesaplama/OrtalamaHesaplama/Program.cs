2fe9c479258826329700072f00c67fb277651436
Onur <69056862+ogunec@users.noreply.github.com>
2022-11-24 19:59:54 +0300

    Add files via upload

---
+++ OrtalamaHesaplama/OrtalamaHesaplama/Program.cs
@@ -0,0 +1,16 @@
+﻿//Kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların ortalamasını alıp ekrana yazdıran uygulama.
+Console.Write("Sayı: ");
+double length = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int b = 1;
+int toplam = 1;
+for (int i = 3; i <= length; i++)
+{
+    int c = a + b;
+    toplam += a + b;
+    a = b;
+    b = c;
+
+}
+double ortalama = toplam/length;
+Console.WriteLine($"Ortalama = {ortalama}");
