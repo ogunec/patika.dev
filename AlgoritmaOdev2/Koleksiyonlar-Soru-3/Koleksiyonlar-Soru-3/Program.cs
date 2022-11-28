91c5d5995b1a7ebf290b3c96c693d9cfe083aead
Onur Güneç <69056862+ogunec@users.noreply.github.com>
2022-11-28 03:33:57 +0300

    Add files via upload

---
+++ AlgoritmaOdev2/Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] sesliler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+List<char> arr = new List<char>(){ };
+Console.Write("Cümle giriniz: ");
+string sentence = Console.ReadLine().ToLower();
+foreach (char character in sentence)
+{
+    foreach (char item in sesliler)
+    {
+        if (character == item)
+        {
+            arr.Add(character);
+        }
+    }
+}
+foreach (char item in arr)
+{
+    Console.Write(item);
+}
